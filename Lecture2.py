61bf5f13b8dd312440b7df2bc6e329b1b2d78ad3
ameliecazelais <ameliecazelais@uvic.ca>
2026-01-08 09:52:10 -0800

    Add files via upload

---
+++ Lecture2.py
@@ -0,0 +1,21 @@
+from machine import Pin
+import time
+
+current = time.time() #for timing
+
+# setup
+led = Pin(12, Pin.OUT)
+button = Pin(14, Pin.IN, Pin.PULL_UP)
+
+while True:
+#     led.value(0)
+#     time.sleep(0.5)
+#     led.value(1)
+#     time.sleep(0.5)
+    if button.value() == 0:
+        led.value(1)
+    else:
+        led.value(0)
+    time.sleep(0.1)
+    
+elapsed = time.time() - current #for timing
