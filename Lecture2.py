8cb7f72dce433b14a6068e6a9cf270655809f0ad
KuljitKaur6 <kuljitkaurbaatth6@uvic.ca>
2026-01-08 09:53:48 -0800

    Update Lecture2.py

--- Lecture2.py
+++ Lecture2.py
@@ -1,14 +1,14 @@
 from machine import Pin
 import time
 
-current = time.time() #for timing
+current = time.time() #for timing - LED lights with button
 
 # setup
 led = Pin(12, Pin.OUT)
 button = Pin(14, Pin.IN, Pin.PULL_UP)
 
 while True:
-#     led.value(0)
+#     led.value(0) - LED lights no button
 #     time.sleep(0.5)
 #     led.value(1)
 #     time.sleep(0.5)
